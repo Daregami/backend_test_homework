20cfc7837ec6d5e16982b55bcbe078d1353e4ec5
Дмитрий Бурдейный <burdeynyy-2004@mail.ru>
2024-10-31 13:13:50 +0300

    Create practicum.py

---
+++ practicum.py
@@ -0,0 +1,13 @@
+"""Эта программа складывает два числа."""
+
+
+def add(local_num1, local_num2):
+    """Функция складывает два числа."""
+    return local_num1 + local_num2
+
+
+NUM1: int = 10
+NUM2: int = 5
+
+print('Сумма чисел:', add(NUM1, NUM2))
+# print(__annotations__)
